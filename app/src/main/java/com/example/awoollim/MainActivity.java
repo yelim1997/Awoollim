fc4d41b8392027a9d73b1fc63a5e777ba19afe80
jeongsoon0111 <psg12302004@gmail.com>
2019-04-05 19:06:56 +0900

    기본 ui 제작

--- app/src/main/java/com/example/awoollim/MainActivity.java
+++ app/src/main/java/com/example/awoollim/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.awoollim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.btn1);
+        Button btn2 = (Button)findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(getApplicationContext(),Main3Activity.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
