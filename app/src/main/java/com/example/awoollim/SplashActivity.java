c0c39c2c27eedd48d64472c31f68208e13bf85d5
jeongsoon0111 <psg12302004@gmail.com>
2019-04-18 10:43:23 +0900

    ui 수정, splash 화면 제작

---
+++ app/src/main/java/com/example/awoollim/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.awoollim;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(2500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
+
