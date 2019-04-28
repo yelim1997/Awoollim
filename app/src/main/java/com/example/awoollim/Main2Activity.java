65e77221100a17e2b5e4625700c81f2e861e4910
yelim1997 <38120531+yelim1997@users.noreply.github.com>
2019-04-28 21:40:38 +0900

    영상->자막업로드4 (미리보기 화면 비율, 미리화기 화면 회전 개선) (추후에 비디오 사이즈 수정할 예정)

--- app/src/main/java/com/example/awoollim/Main2Activity.java
+++ app/src/main/java/com/example/awoollim/Main2Activity.java
@@ -20,41 +20,37 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import java.io.IOException;
 import java.util.List;
 
 public class Main2Activity extends AppCompatActivity implements SurfaceHolder.Callback{
 
-    public static final String TAG = "MainActivity";
-
     private static String EXTERNAL_STORAGE_PATH = "";
     private static String RECORDED_FILE = "video_recorded";
-    private static int fileIndex = 0;
     private static String filename = "";
-
+    private static int fileIndex = 0;
+    private Camera camera = null;
 
     MediaPlayer player;
     MediaRecorder recorder;
-
-    private Camera camera = null;
     SurfaceHolder holder;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
-
         String state = Environment.getExternalStorageState();
-        if (!state.equals(Environment.MEDIA_MOUNTED)) {
-        } else {
+
+        if (!state.equals(Environment.MEDIA_MOUNTED)) { }
+        else
+            {
             EXTERNAL_STORAGE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
-        }
+            }
 
         setContentView(R.layout.activity_main2);
-
         SurfaceView surfaceView = (SurfaceView) findViewById(R.id.videoLayout);
-
 
         holder = surfaceView.getHolder();
         holder.addCallback(this);
@@ -66,27 +62,29 @@ public class Main2Activity extends AppCompatActivity implements SurfaceHolder.Ca
         recordBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 try {
-                    if (recorder == null) {
+                    if (recorder == null)
+                    {
                         recorder = new MediaRecorder();
                     }
 
+                    camera.unlock();
+                    recorder.setCamera(camera);
 
                     recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                    recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                    recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
                     recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
 
                     CamcorderProfile profile = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
                     recorder.setVideoEncodingBitRate(profile.videoBitRate);
                     recorder.setVideoFrameRate(30);
                     recorder.setVideoSize(1000000000,1000000000);
-
                     recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
                     recorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+                    recorder.setOrientationHint(90);
 
                     filename = createFilename();
                     recorder.setOutputFile(filename);
 
-                    recorder.setOrientationHint(90);
                     recorder.setPreviewDisplay(holder.getSurface());
                     recorder.prepare();
                     recorder.start();
@@ -119,142 +117,165 @@ public class Main2Activity extends AppCompatActivity implements SurfaceHolder.Ca
                 values.put(MediaStore.Audio.Media.DATA, filename);
 
                 Uri videoUri = getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
-                if (videoUri == null) {
+                if (videoUri == null)
+                {
                     return;
                 }
 
                 sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, videoUri));
-
             }
         });
-
         checkDangerousPermissions();
     }
 
-    public void surfaceCreated(SurfaceHolder holder) {
+
+    public void surfaceCreated(SurfaceHolder holder)
+    {
         camera = Camera.open();
         camera.setDisplayOrientation(90);
 
         try {
-            if (camera == null) {
+            if (camera == null)
+            {
                 camera.setPreviewDisplay(holder);
                 camera.startPreview();
             }
-        } catch (IOException e) {
-        }
+        } catch (IOException e) { }
     }
 
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
-        if (holder.getSurface() == null) {
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
+    {
+        if (holder.getSurface() == null)
+        {
             return;
         }
-        try {
+        try
+        {
             camera.stopPreview();
-        } catch (Exception e) {
-
-        }
+        } catch (Exception e) { }
 
         Camera.Parameters parameters = camera.getParameters();
         List<String> focusModes = parameters.getSupportedFocusModes();
-        if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+        if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO))
+        {
             parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
         }
         camera.setParameters(parameters);
         try {
             camera.setPreviewDisplay(holder);
             camera.startPreview();
-        } catch (Exception e) {
-        }
+        } catch (Exception e) { }
     }
 
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        if (camera != null) {
+
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
+        if (camera != null)
+        {
             camera.stopPreview();
             camera.release();
             camera = null;
         }
     }
 
-    private void checkDangerousPermissions() {
+
+    private void checkDangerousPermissions()
+    {
         String[] permissions = {
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.RECORD_AUDIO,
-                Manifest.permission.CAMERA
-        };
+                Manifest.permission.CAMERA};
 
         int permissionCheck = PackageManager.PERMISSION_GRANTED;
-        for (int i = 0; i < permissions.length; i++) {
+        for (int i = 0; i < permissions.length; i++)
+        {
             permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
-            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            if (permissionCheck == PackageManager.PERMISSION_DENIED)
+            {
                 break;
             }
         }
 
-        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED)
+        {
             Toast.makeText(this, "권한 있음", Toast.LENGTH_LONG).show();
-        } else {
+        }
+        else
+            {
             Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0]))
+            {
                 Toast.makeText(this, "권한 설명 필요함.", Toast.LENGTH_LONG).show();
-            } else {
-                ActivityCompat.requestPermissions(this, permissions, 1);
             }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == 1) {
-            for (int i = 0; i < permissions.length; i++) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, permissions[i] + " 권한이 승인됨.", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(this, permissions[i] + " 권한이 승인되지 않음.", Toast.LENGTH_LONG).show();
+            else
+                {
+                ActivityCompat.requestPermissions(this, permissions, 1);
                 }
             }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        if (requestCode == 1)
+        {
+            for (int i = 0; i < permissions.length; i++)
+            {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(this, permissions[i] + " 권한이 승인됨.", Toast.LENGTH_LONG).show();
+                }
+                else
+                    {
+                    Toast.makeText(this, permissions[i] + " 권한이 승인되지 않음.", Toast.LENGTH_LONG).show();
+                    }
+            }
         }
     }
 
 
-
-
-
-
-    private String createFilename() {
+    private String createFilename()
+    {
         fileIndex++;
 
         String newFilename = "";
-        if (EXTERNAL_STORAGE_PATH == null || EXTERNAL_STORAGE_PATH.equals("")) {
+        if (EXTERNAL_STORAGE_PATH == null || EXTERNAL_STORAGE_PATH.equals(""))
+        {
             newFilename = RECORDED_FILE + fileIndex + ".mp4";
-        } else {
-            newFilename = EXTERNAL_STORAGE_PATH + "/" + RECORDED_FILE + fileIndex + ".mp4";
         }
+        else
+            {
+            newFilename = EXTERNAL_STORAGE_PATH + "/" + RECORDED_FILE + fileIndex + ".mp4";
+            }
 
         return newFilename;
     }
 
 
-
-    protected void onPause() {
-        if (camera != null) {
+    protected void onPause()
+    {
+        if (camera != null)
+        {
             camera.release();
             camera = null;
         }
 
-        if (recorder != null) {
+        if (recorder != null)
+        {
             recorder.release();
             recorder = null;
         }
 
-        if (player != null) {
+        if (player != null)
+        {
             player.release();
             player = null;
         }
 
         super.onPause();
     }
-
 }
