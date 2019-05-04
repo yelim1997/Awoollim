56d1d2690ffadf1b29fb7ed8d5468cb638c1ffc2
yelim1997 <38120531+yelim1997@users.noreply.github.com>
2019-05-04 18:24:23 +0900

    레이아웃 1차 수정 (이미지 버튼 추가해야함) (로딩이미지 변경해야함)

--- app/src/main/java/com/example/awoollim/MainActivity.java
+++ app/src/main/java/com/example/awoollim/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.awoollim;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn1 = (Button)findViewById(R.id.btn1);
         Button btn2 = (Button)findViewById(R.id.btn2);
+        Button btn3 = (Button)findViewById(R.id.btn3);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent2 = new Intent(getApplicationContext(),Main3Activity.class);
                 startActivity(intent2);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(getApplicationContext(),Main4Activity.class);
+                startActivity(intent3);
             }
         });
     }
